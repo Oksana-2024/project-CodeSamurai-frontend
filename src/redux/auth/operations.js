72a0245d13a5f28702b5dbd72c08f9622cbbf39a
Sasha Pavluk <bogota3443@gmail.com>
2025-04-23 10:49:10 +0300

    logout

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -4,3 +4,19 @@
 //import { clearAuthHeader } from "../../service/axios";
 
 //baseAxios.post = axios.post
+
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const logoutUser = createAsyncThunk(
+  "auth/logout",
+  async (_, thunkApi) => {
+    try {
+      await axios.post("/api/logout");
+      localStorage.removeItem("token");
+      return;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
