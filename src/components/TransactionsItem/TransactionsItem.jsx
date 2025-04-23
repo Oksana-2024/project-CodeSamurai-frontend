9400ab37f48c5ef8553365ad51ee4347a8704a78
Yuliia Romanovych <jdani22101998@gmail.com>
2025-04-23 23:42:24 +0300

    added lists styles for mobile size

--- src/components/TransactionsItem/TransactionsItem.jsx
+++ src/components/TransactionsItem/TransactionsItem.jsx
@@ -4,16 +4,21 @@ import {deleteTransactions} from "../../redux/transactions/operations";
 import s from "./TransactionsItem.module.css";
 import Button from "../Button/Button";
 import {MdOutlineModeEdit} from "react-icons/md";
+import useMedia from "../../helpers/useMedia";
 
-const getStyleByType = (type) => {
-  const currentColor = type === "-" ? "var(--red-color)" : "var(--yellow-color)";
-  return {
-    color: currentColor,
-  };
-};
+const getStyleByType = (type) => (type === "INCOME" ? "var(--income-color)" : "var(--expense-color)");
 
 function TransactionItem({transaction, id}) {
   const dispatch = useDispatch();
+  const {isMobile} = useMedia();
+
+  const {transactionDate, type, comment, amount} = transaction;
+  const date = new Date(transactionDate);
+  const formatedDate = `${String(date.getDate()).padStart(2, "0")}
+  .${String(date.getMonth() + 1).padStart(2, "0")}
+  .${date.getFullYear().toString().slice(-2)}`;
+  const formatedType = type == "INCOME" ? "+" : "-";
+  const color = getStyleByType(type);
 
   function onEdit() {
     console.log("open edit modal");
@@ -23,9 +28,53 @@ function TransactionItem({transaction, id}) {
     await dispatch(deleteTransactions(id));
   }
 
+  if (isMobile) {
+    return (
+      <div
+        className={s.list}
+        style={{borderColor: color}}>
+        <div className={s.list_item}>
+          <span>Date</span>
+          <span>{formatedDate}</span>
+        </div>
+        <div className={s.list_item}>
+          <span>Type</span>
+          <span>{formatedType}</span>
+        </div>
+        <div className={s.list_item}>
+          <span>Category</span>
+          <span>todo...</span>
+        </div>
+        <div className={s.list_item}>
+          <span>Comment</span>
+          <span>{comment}</span>
+        </div>
+        <div className={s.list_item}>
+          <span>Sum</span>
+          <span>{amount}</span>
+        </div>
+        <div className={s.list_item}>
+          <Button
+            type="button"
+            className={clsx(s.delete_btn)}
+            onClick={OnDelete}
+            text={"Delete"}
+          />
+          <button
+            type="button"
+            className={s.edit_btn}
+            onClick={onEdit}>
+            <MdOutlineModeEdit />
+            Edit
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <tr className={s.row}>
-      <td className={s.row_item}>{transaction.transactionDate}</td>
+      <td className={s.row_item}>{formatedDate}</td>
       <td
         className={s.row_item}
         style={{textAlign: "center"}}>
@@ -36,7 +85,7 @@ function TransactionItem({transaction, id}) {
       <td className={s.row_item}>{transaction.comment}</td>
       <td
         className={s.row_item}
-        style={{textAlign: "right"}}>
+        style={{color}}>
         {transaction.amount}
       </td>
       <td className={clsx(s.row_item, s.controls)}>
--- src/components/TransactionsList/TransactionsList.jsx
+++ src/components/TransactionsList/TransactionsList.jsx
@@ -4,52 +4,76 @@ import s from "./TransactionsList.module.css";
 import Loader from "../Loader/Loader";
 import FormButton from "../Button/Button";
 import TransactionItem from "../TransactionsItem/TransactionsItem";
+import useMedia from "../../helpers/useMedia";
 
 const columns = ["Date", "Type", "Category", "Comment", "Sum", ""];
 
 function TransactionList() {
   const reduxTransactions = useSelector(selectTransactions);
-  console.log("reduxTransactions", reduxTransactions);
+
+  const {isMobile} = useMedia();
 
   const isLoading = useSelector(selectTransLoading);
   const isError = useSelector(selectTransError);
   const dispatch = useDispatch();
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <p className={s.text}>Oops, something went wrong...</p>;
+  }
+
+  if (reduxTransactions.length === 0 && !isLoading) {
+    return (
+      <div className={s.container}>
+        <p>No transactions available yet.</p>
+        <p>Let&#39;s add your first transaction:</p>
+        <FormButton
+          type="button"
+          text={"Add transaction"}
+          variant={"multiColorButton"}
+          handlerFunction={() => console.log("open modal")}
+        />
+      </div>
+    );
+  }
+
+  if (isMobile) {
+    return (
+      <div className={s.container}>
+        {reduxTransactions.map(({id, ...item}) => (
+          <TransactionItem
+            key={id}
+            id={id}
+            transaction={item}
+          />
+        ))}
+      </div>
+    );
+  }
+
   return (
     <>
-      {isLoading && <Loader />}
-      {isError && <p className={s.text}>Oops, something went wrong...</p>}
-      {!isLoading && reduxTransactions.length === 0 ? (
-        <div className={s.container}>
-          <p>No transactions available yet.</p>
-          <p>Let&#39;s add your first transaction:</p>
-          <FormButton
-            type="button"
-            text={"Add transaction"}
-            variant={"multiColorButton"}
-            handlerFunction={() => console.log("open modal")}
-          />
-        </div>
-      ) : (
-        <table className={s.table}>
-          <thead className={s.head_row}>
-            <tr className={s.row_item}>
-              {columns.map((column, idx) => (
-                <th key={idx}>{column}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {reduxTransactions.map(({id, ...item}) => (
-              <TransactionItem
-                key={id}
-                id={id}
-                transaction={item}
-              />
+      <table className={s.table}>
+        <thead className={s.head_row}>
+          <tr className={s.row_item}>
+            {columns.map((column, idx) => (
+              <th key={idx}>{column}</th>
             ))}
-          </tbody>
-        </table>
-      )}
+          </tr>
+        </thead>
+        <tbody>
+          {reduxTransactions.map(({id, ...item}) => (
+            <TransactionItem
+              key={id}
+              id={id}
+              transaction={item}
+            />
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
