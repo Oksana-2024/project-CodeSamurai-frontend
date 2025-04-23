c4f284bb70458594159f05bae3e7166e46ceab66
Yuliia Romanovych <jdani22101998@gmail.com>
2025-04-23 20:46:42 +0300

    get list

--- src/pages/DashboardPage/HomeTab/HomeTab.jsx
+++ src/pages/DashboardPage/HomeTab/HomeTab.jsx
@@ -1,10 +1,19 @@
-import { useMediaQuery } from "react-responsive";
+import {useMediaQuery} from "react-responsive";
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
 import Balance from "../../../components/Balance/Balance";
 import TransactionsList from "../../../components/TransactionsList/TransactionsList";
 import s from "./HomeTab.module.css";
+import {getTransactions} from "../../../redux/transactions/operations"; // Adjust the path as needed
 
 const HomeTab = () => {
-  const isSmallScreen = useMediaQuery({ query: "(max-width: 767px)" });
+  const isSmallScreen = useMediaQuery({query: "(max-width: 767px)"});
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTransactions());
+  }, [dispatch]);
 
   return (
     <div>
