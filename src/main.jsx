b7b98479d45897329d1ba5f75e080b4e6013c240
Dmtro_Tr <Di4gamegem@gmail.com>
2025-04-27 15:54:09 +0300

    fix addmodal addTransactionForm

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,13 @@ import "./index.css";
 import App from "../src/components/App.jsx";
 
 import { persistor, store } from "./redux/store.js";
-import ModalAddTransaction from "./components/ModalAddTransaction/ModalAddTransaction.jsx";
-import AddTransactionForm from "./components/AddTransactionForm/AddTransactionForm.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          {/* <App /> */}
-          <ModalAddTransaction />
-          <AddTransactionForm />
+          <App />
         </PersistGate>
       </Provider>
     </BrowserRouter>
