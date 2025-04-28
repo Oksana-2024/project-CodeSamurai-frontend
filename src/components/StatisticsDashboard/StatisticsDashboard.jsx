5f1bf344e175c2172f9c0cf9054d95134df361ab
kostiantyn_ivanchenko <kokos2702@gmail.com>
2025-04-28 15:11:27 +0200

    stats-table

--- src/components/StatisticsDashboard/StatisticsDashboard.jsx
+++ src/components/StatisticsDashboard/StatisticsDashboard.jsx
@@ -1,9 +1,11 @@
-import Container from "../Container/Container"
+import Container from "../Container/Container";
 
 const StatisticsDashboard = () => {
   return (
-    <div><Container>StatisticsDashboard</Container></div>
-  )
-}
+    <div>
+      <Container>StatisticsDashboard</Container>
+    </div>
+  );
+};
 
-export default StatisticsDashboard
+export default StatisticsDashboard;
--- src/components/StatisticsTable/StatisticsTable.jsx
+++ src/components/StatisticsTable/StatisticsTable.jsx
@@ -1,7 +1,48 @@
-const StatisticsTable = () => {
-  return (
-    <div>StatisticsTable</div>
-  )
-}
+import { useSelector } from "react-redux";
+import styles from "./StatisticsTable.module.css";
 
-export default StatisticsTable
+const StatisticsTable = () => {
+  const { statistics } = useSelector((state) => state.statistics);
+
+  if (!statistics?.categories?.length) {
+    return (
+      <p className={styles.message}>No statistics available for this period.</p>
+    );
+  }
+
+  return (
+    <div className={styles.tableWrapper}>
+      <ul className={styles.table}>
+        {statistics.categories.map((category) => (
+          <li key={category.name} className={styles.row}>
+            <span
+              className={styles.color}
+              style={{ backgroundColor: category.color }}
+            ></span>
+            <span className={styles.name}>{category.name}</span>
+            <span className={styles.amount}>
+              {category.total.toFixed(2)} UAH
+            </span>
+          </li>
+        ))}
+      </ul>
+
+      <div className={styles.total}>
+        <p>
+          Expenses:{" "}
+          <span className={styles.expenses}>
+            {statistics.expenseSummary.toFixed(2)} UAH
+          </span>
+        </p>
+        <p>
+          Income:{" "}
+          <span className={styles.income}>
+            {statistics.incomeSummary.toFixed(2)} UAH
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StatisticsTable;
