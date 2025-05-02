bbd6d7af98ddedeb6ce1dc4ff169589b9feb695b
Oksana <oksanavasilevna77@gmail.com>
2025-05-02 23:14:16 +0300

    Updated user schema, corrected paddings on dashboard, and refined decorative line style

--- src/helpers/userSchema.js
+++ src/helpers/userSchema.js
@@ -4,6 +4,8 @@ export const validationSchemaUserUpdate = object({
   name: string()
     .min(2, "Minimum length is 2 characters")
     .max(20, "Maximum length is 20 characters")
-    .matches(/^[A-Za-z]+$/, { message: "You can add only letters" })
+    .matches(/^[a-zA-Zа-щА-ЩЬьЮюЯяІіЇїЄєҐґ]+$/, {
+      message: "You can add only letters",
+    })
     .required("Required"),
 });
